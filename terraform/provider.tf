0a287484b075b30a31abc2b91db2d75caa8600bc
schmillin <me@showmewhatyougot.live>
2023-10-16 05:28:03 +0000

    fix

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,14 +8,12 @@ terraform {
     }
   }
 
-  terraform {
     required_providers {
       aws = {
         source  = "hashicorp/aws"
         version = "5.21.0"
       }
     }
-  }
 
 
   required_version = ">= 1.1.0"
