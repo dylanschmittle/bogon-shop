d298ff9317417e7ec8af02e080235bcbfecfc84c
schmillin <me@showmewhatyougot.live>
2023-10-16 05:30:37 +0000

    hide

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,38 +1,38 @@
-module "eks_blueprints_addons" {
-  source  = "aws-ia/eks-blueprints-addons/aws"
-  version = "~> 1.0" #ensure to update this to the latest/desired version
-
-  cluster_name      = "my-cluster"
-  cluster_endpoint  = "public"
-  cluster_version   = "1.27"
-  oidc_provider_arn = "arn:aws:iam::754977136847:policy/AWSLoadBalancerControllerIAMPolicy"
-
-  eks_addons = {
-    aws-ebs-csi-driver = {
-      most_recent = true
-    }
-    coredns = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-  }
-  enable_ingress_nginx                   = true
-  enable_aws_load_balancer_controller    = true
-  enable_cluster_proportional_autoscaler = true
-  enable_karpenter                       = true
-  enable_kube_prometheus_stack           = true
-  enable_metrics_server                  = true
-  enable_external_dns                    = true
-  enable_cert_manager                    = true
-  cert_manager_route53_hosted_zone_arns  = ["arn:aws:route53:::hostedzone/Z08477012TVCGJOXMCH3Q"]
-
-  tags = {
-    Root_Domain = "my.bogon.shop"
-    Environment = "dev"
-  }
-}
+# module "eks_blueprints_addons" {
+#   source  = "aws-ia/eks-blueprints-addons/aws"
+#   version = "~> 1.0" #ensure to update this to the latest/desired version
+#
+#   cluster_name      = "my-cluster"
+#   cluster_endpoint  = "public"
+#   cluster_version   = "1.27"
+#   oidc_provider_arn = "arn:aws:iam::754977136847:policy/AWSLoadBalancerControllerIAMPolicy"
+#
+#   eks_addons = {
+#     aws-ebs-csi-driver = {
+#       most_recent = true
+#     }
+#     coredns = {
+#       most_recent = true
+#     }
+#     vpc-cni = {
+#       most_recent = true
+#     }
+#     kube-proxy = {
+#       most_recent = true
+#     }
+#   }
+#   enable_ingress_nginx                   = true
+#   enable_aws_load_balancer_controller    = true
+#   enable_cluster_proportional_autoscaler = true
+#   enable_karpenter                       = true
+#   enable_kube_prometheus_stack           = true
+#   enable_metrics_server                  = true
+#   enable_external_dns                    = true
+#   enable_cert_manager                    = true
+#   cert_manager_route53_hosted_zone_arns  = ["arn:aws:route53:::hostedzone/Z08477012TVCGJOXMCH3Q"]
+#
+#   tags = {
+#     Root_Domain = "my.bogon.shop"
+#     Environment = "dev"
+#   }
+# }
