02930605111e73fc350a4ce52d5dba0c521b5091
schmillin <me@showmewhatyougot.live>
2023-10-17 14:49:38 +0000

    fix

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.38"
+      version = ">= 3.74.0"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
