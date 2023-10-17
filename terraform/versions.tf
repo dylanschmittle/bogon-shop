9664630e3b95893c486fd64dacaa7312db363cf1
schmillin <me@showmewhatyougot.live>
2023-10-17 16:56:51 +0000

    helm

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,6 +2,10 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.2"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.7.1"
